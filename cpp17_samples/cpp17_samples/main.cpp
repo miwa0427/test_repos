1dd0d7b7e1b45cdbf70c9a2356903d26afc03703
mac <max2digitway@outlook.jp>
2018-08-25 22:52:43 +0900

    並列実行のサンプルを登録
    main()関数に直接書いているのはあくまで暫定
    その内、項目毎にディレクトリへ分ける

--- cpp17_samples/cpp17_samples/main.cpp
+++ cpp17_samples/cpp17_samples/main.cpp
@@ -3,12 +3,41 @@
 
 #include "pch.h"
 
-using namespace winrt;
-using namespace Windows::Foundation;
+#include <iostream>
+#include <algorithm>
+#include <execution>
+
+template < typename T, typename T2 >
+bool check_container_contents( T const & container, T2 pred )
+{
+    return std::all_of( std::execution::par,
+                        std::begin( container ), std::end( container ),
+                        pred );
+}
 
 int main()
 {
-    init_apartment();
-    Uri uri(L"http://aka.ms/cppwinrt");
-    printf("Hello, %ls!\n", uri.AbsoluteUri().c_str());
+    std::vector< int > vec;
+    for( int i = 0; i < 100; i++ )
+        vec.push_back( i );
+
+    auto less_than100 = []( auto x ){ return x < 100; };
+    auto is_even_num = []( auto x ){ return ( ( x % 2 ) == 0 ) ; };
+    auto is_odd_num = []( auto x ){ return ( ( x % 2 ) != 0 ); };
+
+    printf( "コンテナの全要素は100より小さい？ -> %s\n", check_container_contents( vec, less_than100 ) ? "真" : "偽" );
+    printf( "コンテナの全要素は偶数？          -> %s\n", check_container_contents( vec, is_even_num ) ? "真" : "偽" );
+    printf( "コンテナの全要素は奇数？          -> %s\n", check_container_contents( vec, is_odd_num ) ? "真" : "偽" );
+
+    // 詰め直す
+    printf( "-----------------------------------------\n"
+            "コンテナ詰め直し\n"
+            "-----------------------------------------\n");
+    vec.clear();
+    for( int i = 0; i < 100; i += 2 )
+        vec.push_back( i );
+
+    printf( "コンテナの全要素は100より小さい？ -> %s\n", check_container_contents( vec, less_than100 ) ? "真" : "偽" );
+    printf( "コンテナの全要素は偶数？          -> %s\n", check_container_contents( vec, is_even_num ) ? "真" : "偽" );
+    printf( "コンテナの全要素は奇数？          -> %s\n", check_container_contents( vec, is_odd_num ) ? "真" : "偽" );
 }
