abd7e3aaade0bfe7d50ef7db3bcc28acfb2119d4
mac <max2digitway@outlook.jp>
2018-09-01 10:13:40 +0900

    shared_ptr/addressofサンプル追加
    CRTのsecure置き換えが面倒なので、一旦_CRT_SECURE_NO_WARNINGSで逃げる
    std::sharted_ptrのカスタムデリータを試す
    IOマニピュレータによる基数指定

--- cpp17_samples/cpp17_samples/main.cpp
+++ cpp17_samples/cpp17_samples/main.cpp
@@ -3,17 +3,34 @@
 
 #include "pch.h"
 
+#include <iostream>
+
 void para_sample1( void );
 void map_sample( void );
 void map_sample2( void );
 void tuple_sample( void );
 void lambda_sample();
+void util_sample();
+void smart_ptr_sample();
 
 int main()
 {
+    std::cout << "\n\n\n=== Paralells sample ===" << std::endl;
     para_sample1();
+
+    std::cout << "\n\n\n=== map sample ===" << std::endl;
     map_sample();
     map_sample2();
+
+    std::cout << "\n\n\n=== tuple sample ===" << std::endl;
     tuple_sample();
+
+    std::cout << "\n\n\n=== lambda sample ===" << std::endl;
     lambda_sample();
+
+    std::cout << "\n\n\n=== addressof sample ===" << std::endl;
+    util_sample();
+
+    std::cout << "\n\n\n=== custom deleter sample ===" << std::endl;
+    smart_ptr_sample();
 }
