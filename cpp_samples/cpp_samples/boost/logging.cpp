76ae1ef318e9d46a574fe2a36b6adea903951799
mac <max2digitway@outlook.jp>
2018-09-04 15:20:04 +0900

    boost::logサンプル追加。VSのバージョン更新
    boostをvcpkg(https://docs.microsoft.com/ja-jp/cpp/vcpkg)で導入するも、x64を見つけられなかったのでx86版を使用
    Windows SDK 17134導入後、WinRTのプロジェクトテンプレートがビルドエラーとなる事が発覚。既知の問題でコマンドラインオプション"/Zc:twoPhase- "を追加(2フェーズの名前検索を無効化する機能との事)
    <https://stackoverflow.com/questions/51066729/c-winrt-part-of-windows-sdk-17134-is-not-compatible-with-visual-studio-15-8-p>

---
+++ cpp_samples/cpp_samples/boost/logging.cpp
@@ -0,0 +1,25 @@
+﻿#include "pch.h"
+
+#include <boost/log/trivial.hpp>
+#include <boost/log/core.hpp>
+#include <boost/log/utility/setup/file.hpp>
+#include <boost/log/expressions.hpp>
+
+void boost_logging_sample()
+{
+    // サンプルコードはboost.orgのものを拝借
+    using namespace boost::log;
+
+    // ロギングファイル設定
+    add_file_log( R"*(./test_dir/boost_log.txt)*" );
+
+    // ロギングレベル(warning)以上のものをロギング対象に設定
+    core::get()->set_filter( trivial::severity >= trivial::warning );
+
+    BOOST_LOG_TRIVIAL( trace ) << "A trace severity message";
+    BOOST_LOG_TRIVIAL( debug ) << "A debug severity message";
+    BOOST_LOG_TRIVIAL( info ) << "An informational severity message";
+    BOOST_LOG_TRIVIAL( warning ) << "A warning severity message";
+    BOOST_LOG_TRIVIAL( error ) << "An error severity message";
+    BOOST_LOG_TRIVIAL( fatal ) << "A fatal severity message";
+}
